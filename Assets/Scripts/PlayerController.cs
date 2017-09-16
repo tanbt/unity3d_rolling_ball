eb403e5cae7865f9f7aa3f05c61ecc3b9989c828
Tan Bui at home <tan.bui.home@gmail.com>
2017-09-16 21:24:29 +0300

    Show textes and count.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed = 10.0f;
 
     private Rigidbody rb;
+    private int totalItems;
+
+    private int count;
+    public Text countText;
+    public Text winText;
 
     // Call at the very first frame
     private void Start()
     {
         // Access the Rigidbody component which is attached to the current game object
         rb = GetComponent<Rigidbody>();
+        totalItems = GameObject.FindGameObjectsWithTag("Pick up").Length;
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Call before a physic effetc
@@ -31,6 +41,22 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            UpdateWinText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "<i>Count: <b>" + count.ToString() + " / " + totalItems.ToString() + "</b></i>";
+    }
+
+    private void UpdateWinText()
+    {
+        if (count == totalItems)
+        {
+            winText.text = "<b>YOU WIN!</b>";
         }
     }
 
